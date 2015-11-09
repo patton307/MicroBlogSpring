9b455ab54c37c224f2e845906ef2008b09f26b51
landonkail <landon.kail@gmail.com>
2015-11-09 18:58:27 -0500

    initial commit

---
+++ src/main/java/com/theironyard/Message.java
@@ -0,0 +1,35 @@
+package com.theironyard;
+
+/**
+ * Created by landonkail on 11/9/15.
+ */
+public class Message {
+    String text;
+    int id;
+
+    public Message(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public Message() {
+
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
---
+++ src/main/java/com/theironyard/MessageController.java
@@ -0,0 +1,66 @@
+package com.theironyard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by landonkail on 11/9/15.
+ */
+
+@Controller
+public class MessageController {
+
+    ArrayList<Message> list = new ArrayList<>();
+
+    // Home
+    @RequestMapping("/")
+    public String home(Model model, HttpServletRequest request) {
+        try {
+            HttpSession session = request.getSession();
+            String username = (String) session.getAttribute("username");
+            model.addAttribute("username", username);
+            model.addAttribute("message", list);
+        } catch (Exception e) {}
+        return "home";
+    }
+
+
+    // NO USER - INITIAL LOGIN
+    @RequestMapping("/login")
+    public String createUser(HttpServletRequest request, String username) {
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+
+
+    // INSERT MESSAGE
+    @RequestMapping("/create-message")
+    public String createMessage (String text) {
+        int id = list.size() + 1;
+        Message m = new Message(id, text);
+        list.add(m);
+        return "redirect:/";
+    }
+
+    // DELETE MESSAGE
+    @RequestMapping("/delete-message")
+    public String deleteMessage (HttpServletRequest request, int id) {
+        list.remove(id -1);
+        int idNum = 1;
+        for (Message m : list) {
+            m.id = idNum;
+            idNum++;
+        }
+        return "redirect:/";
+    }
+}
